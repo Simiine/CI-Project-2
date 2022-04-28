ef5d4c61219b3fea03604b6574b83145734f0022
Simi <simiinemboj@gmail.com>
2022-04-28 19:14:31 +0000

    Add javascript notes

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Questions
+ */
+
+/**
+ * Intro Screen
+ */
+
+/**
+ * Loop through the questions and present with answers
+ */
+
+/**
+ * Select Answers
+ */
+
+/**
+ * Check if answer is correct
+ */
+
+/**
+ * Handle score
+ */
+
+/**
+ * keep looping
+ */
+
+/**
+ * When no more questions
+ * end game and present options
+ */
