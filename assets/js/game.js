1ccc0cac6d925f794bcc09f0f677195340f88be6
Simi <simiinemboj@gmail.com>
2022-04-29 20:14:10 +0000

    Add event listners for answer options

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,16 +67,40 @@ startGame = () => {
 
 // Select random questions from available
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        //go to the end page once gone through all questions
+        return window.location.assign("/end.html");
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
+    // Lists choices from dataset
     choices.forEach( choice => {
         const number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
     });
+
+    //Remove the question just used so it won't repeat
+    availableQuestion.splice(questionIndex, 1);
+
+    // allow users to answer
+    acceptingAnswers = true;
 };
+
+// Add event listener for clicking answer options
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+       console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
 
 startGame();
 
