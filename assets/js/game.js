416a13c64df1f716622f0fb76433fc27e41a79d9
Simi <simiinemboj@gmail.com>
2022-04-29 14:45:03 +0000

    Add let variables to Javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,35 @@
-// Contants
+// Contant 
 
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(choices);
+
+// Let variables
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
+
+let questions = [
+    {
+        question: "Test Question",
+        choice1: "Test Answer 1",
+        choice2: "Test Answer 2",
+        choice3: "Test Answer 3",
+        choice4: "Test Answer 4",
+        answer: 1
+    },
+    {
+        question: "Test Question",
+        choice1: "Test Answer 1",
+        choice2: "Test Answer 2",
+        choice3: "Test Answer 3",
+        choice4: "Test Answer 4",
+        answer: 3
+    }
+];
 
 /**
  * Questions
