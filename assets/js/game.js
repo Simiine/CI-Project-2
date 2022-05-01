deafa72ddd29fc8151fc3b52ce155d3fb6610292
Simi <simiinemboj@gmail.com>
2022-05-01 19:23:57 +0000

    Add Javascript for progress bar

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 
 /**
@@ -75,7 +75,7 @@ getNewQuestion = () => {
     questionCounter++;
 
     // shows number of questions user is on out of total
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
