3a21f0db48e4ef1466d11ee7137719703fd5a898
Simi <simiinemboj@gmail.com>
2022-05-02 20:02:50 +0000

    Reorder constant variables end page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,11 +1,9 @@
 //Constant Variables
-const username = document.getElementById('username');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
-
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
+const username = document.getElementById('username');
 const MAX_HIGH_SCORE = 5;
 
 finalScore.innerText = mostRecentScore;
