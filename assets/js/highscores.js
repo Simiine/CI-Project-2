4321a672ee0d4d49ccf7b29eede4e0550dad027d
Simi <simiinemboj@gmail.com>
2022-05-04 20:19:27 +0000

    Rewrite function for high scores page to write no no high scores when empty

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,10 +1,16 @@
 //Constant variables
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores')) || undefined;
 const highScoresList = document.getElementById('highScoresList');
 
-//Displays high scores
-highScoresList.innerHTML = 
-    highScores.map(score => {
-        return `<li class="high-score">${score.name} - ${score.score}</li>`;
-    })
-    .join("");
+function mapScores() {
+    if (!highScores) {
+        highScoresList.innerHTML = 'There are no scores!'
+    } else {
+        highScoresList.innerHTML = highScores.map(score => {
+            return `<li class="high-score">${score.name} - ${score.score}</li>`;
+        })
+        .join("");
+    }
+}
+
+mapScores()
