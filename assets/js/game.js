ba1eeb85ead5dfde3f4e981e21f500f697d9a05c
Simi <simiinemboj@gmail.com>
2022-05-04 19:06:21 +0000

    Fix links

--- assets/js/game.js
+++ assets/js/game.js
@@ -117,7 +117,7 @@ function getNewQuestion() {
 
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions){
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign("/end.html");
+        return window.location.assign("end.html");
     }
 
     questionCounter++;
